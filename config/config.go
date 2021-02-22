32cc47ce05f9518cebbd3f80fa44c3967687a576
Toyoaki Doi <kokorononakaniitumo@yahoo.co.jp>
2021-02-22 17:18:07 +0900

    release

--- config/config.go
+++ config/config.go
@@ -1,28 +1,23 @@
 package config
 
+import "github.com/kelseyhightower/envconfig"
+
 type Config struct {
 	DB Db
 }
 
 type Db struct {
-	DbUserName string
-	DbUserPassword string
-	DbHost string
-	DbPort string
-	DbName string
+	DbUserName     string `envconfig:"MYSQL_USER"`
+	DbUserPassword string `envconfig:"MYSQL_PASSWORD"`
+	DbHost         string `envconfig:"MYSQL_HOST"`
+	DbPort         string `envconfig:"MYSQL_PORT"`
+	DbName         string `envconfig:"MYSQL_DATABASE"`
 }
+
 func New() (*Config, error) {
-	cfg := &Config{
-		DB: Db{
-			DbUserName: "latona",
-			DbPort:"30000",
-			DbHost: "localhost",
-			DbName: "PeripheralDevice",
-			DbUserPassword: "L@ton@2019!",
-		},
+	cfg := &Config{}
+	if err := envconfig.Process("", cfg); err != nil {
+		return nil, err
 	}
-	//if err := envconfig.Process("", cfg); err != nil {
-	//	return nil, err
-	//}
 	return cfg, nil
 }
